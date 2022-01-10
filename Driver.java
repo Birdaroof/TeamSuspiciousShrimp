dd0dc2cb987cf1d09d8b15072bb519b07a01ffde
Jomin Zhang <90665454+Birdaroof@users.noreply.github.com>
2022-01-10 15:35:51 -0500

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -21,52 +21,73 @@ public class Driver {
         descend3.add(j);
       }
     //System.out.println(descend);
-
-    ArrayList randomOrder = new ArrayList<Integer>();
-      for (int k = 0; k <= 10; k++){
-        randomOrder.add((int) (Math.random()*11));
-      }
-
-    ArrayList randomOrder2 = new ArrayList<Integer>();
-      for (int l = 0; l < randomOrder.size(); l++){
-        randomOrder2.add(randomOrder.get(l));
-      }
-    //System.out.println(randomOrder2);
-
-    ArrayList randomOrder3 = new ArrayList<Integer>();
-      for (int m = 0; m < randomOrder.size(); m++){
-        randomOrder3.add(randomOrder.get(m));
-      }
+    ArrayList bubbleTest = Sorts.populate(10,0,10);
+    ArrayList bubbleTest1 = Sorts.populate(100,0,100);
+    ArrayList bubbleTest2 = Sorts.populate(1000,0,1000);
+    ArrayList bubbleTest3 = Sorts.populate(10000,0,10000);
     //System.out.println(randomOrder3);
     System.out.println("\nAscending Array: " + ascend);
     System.out.println("Bubble Sort Starting");
     Sorts.bubbleSort(ascend);
-    System.out.println("List after bubble sort: " + ascend );
+    System.out.println("Bubble Sort Finished\n" );
 
     System.out.println("\nDescending Array: " + descend);
     System.out.println("Bubble Sort Starting");
     Sorts.bubbleSort(descend);
-    System.out.println("List after bubble sort: " + descend );
+    System.out.println("Bubble Sort Finished\n" );
 
-    System.out.println("\nRandom Array:" + randomOrder);
-    System.out.println("Bubble Sort Starting");
-    Sorts.bubbleSort(randomOrder);
-    System.out.println("List after bubble sort: " + randomOrder );
-
+    System.out.println("Bubble Sort Starting on random array length 10");
+    Sorts.bubbleSort(bubbleTest);
+    System.out.println("Bubble Sort Finished\n");
+    
+    System.out.println("Bubble Sort Starting on random array length 100");
+    Sorts.bubbleSort(bubbleTest1);
+    System.out.println("Bubble Sort Finished\n" );
+    
+    System.out.println("Bubble Sort Starting on random array length 1000");
+    Sorts.bubbleSort(bubbleTest2);
+    System.out.println("Bubble Sort Finished\n" );
+    
+    System.out.println("Bubble Sort Starting on random array length 10000");
+    Sorts.bubbleSort(bubbleTest3);
+    System.out.println("Bubble Sort Finished\n" );
+    
+    ArrayList insertionTest = Sorts.populate(10,0,10);
+    ArrayList insertionTest1 = Sorts.populate(100,0,100);
+    ArrayList insertionTest2 = Sorts.populate(1000,0,1000);
+    ArrayList insertionTest3 = Sorts.populate(10000,0,10000);
+    
     System.out.println("\nAscending Array: " + ascend2);
     System.out.println("Insertion Sort Starting");
     Sorts.insertionSort(ascend2);
-    System.out.println("List after insertion sort: " + ascend2);
+    System.out.println("Insertion Sort Finished");
 
     System.out.println("\nDescending Array: " + descend2);
     System.out.println("Insertion Sort Starting");
     Sorts.insertionSort(descend2);
-    System.out.println("List after insertion sort: " + descend2);
+    System.out.println("Insertion Sort Finished");
 
-    System.out.println("\nRandom Array:" + randomOrder2);
-    System.out.println("Insertion Sort Starting");
-    Sorts.insertionSort(randomOrder2);
-    System.out.println("List after insertion sort:" + randomOrder2);
+    //System.out.println("\nRandom Array:" + randomOrder2);
+    System.out.println("\nInsertion Sort Starting on random array length 10");
+    Sorts.insertionSort(insertionTest);
+    System.out.println("Insertion Sort Finished");
+
+    System.out.println("\nInsertion Sort Starting on random array length 100");
+    Sorts.insertionSort(insertionTest1);
+    System.out.println("Insertion Sort Finished");
+    
+    System.out.println("\nInsertion Sort Starting on random array length 1000");
+    Sorts.insertionSort(insertionTest2);
+    System.out.println("Insertion Sort Finished");
+    
+    System.out.println("\nInsertion Sort Starting on random array length 10000");
+    Sorts.insertionSort(insertionTest3);
+    System.out.println("Insertion Sort Finished");
+    
+    ArrayList selectionTest = Sorts.populate(10,0,10);
+    ArrayList selectionTest1 = Sorts.populate(100,0,100);
+    ArrayList selectionTest2 = Sorts.populate(1000,0,1000);
+    ArrayList selectionTest3 = Sorts.populate(10000,0,10000);
 
     System.out.println("\nAscending Array: " + ascend3);
     System.out.println("Selection Sort Starting");
@@ -78,9 +99,20 @@ public class Driver {
     Sorts.bubbleSort(descend3);
     System.out.println("List after selection sort: " + descend3 );
 
-    System.out.println("\nRandom Array:" + randomOrder3);
-    System.out.println("Selection Sort Starting");
-    Sorts.selectionSort(randomOrder3);
-    System.out.println("List after selection sort: " + randomOrder3);
+    System.out.println("\nSelection Sort Starting on random array length 10");
+    Sorts.insertionSort(selectionTest);
+    System.out.println("Selection Sort Finished");
+
+    System.out.println("\nSelection Sort Starting on random array length 100");
+    Sorts.insertionSort(selectionTest1);
+    System.out.println("Selection Sort Finished");
+    
+    System.out.println("\nSelection Sort Starting on random array length 1000");
+    Sorts.insertionSort(selectionTest2);
+    System.out.println("Selection Sort Finished");
+    
+    System.out.println("\nSelection Sort Starting on random array length 10000");
+    Sorts.insertionSort(selectionTest3);
+    System.out.println("Selection Sort Finished");
   }
 }
