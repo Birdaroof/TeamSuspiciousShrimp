866198b0faf184cb70c88f4601aedcf8e0a2f521
Jomin <jzhang23@stuy.edu>
2022-01-09 22:44:43 -0500

    w

--- Driver.java
+++ Driver.java
@@ -1,1 +1,86 @@
+import java.util.ArrayList;
+public class Driver {
+  public static void main(String[] args){
+    ArrayList ascend = new ArrayList<Integer>();
+    ArrayList ascend2 = new ArrayList<Integer>();
+    ArrayList ascend3 = new ArrayList<Integer>();
+      for (int i = 0; i <= 10; i++){
+        ascend.add(i);
+        ascend2.add(i);
+        ascend3.add(i);
+      }
 
+    //System.out.println(ascend);
+
+    ArrayList descend = new ArrayList<Integer>();
+    ArrayList descend2 = new ArrayList<Integer>();
+    ArrayList descend3 = new ArrayList<Integer>();
+      for (int j = 10; j >= 0; j--){
+        descend.add(j);
+        descend2.add(j);
+        descend3.add(j);
+      }
+    //System.out.println(descend);
+
+    ArrayList randomOrder = new ArrayList<Integer>();
+      for (int k = 0; k <= 10; k++){
+        randomOrder.add((int) (Math.random()*11));
+      }
+
+    ArrayList randomOrder2 = new ArrayList<Integer>();
+      for (int l = 0; l < randomOrder.size(); l++){
+        randomOrder2.add(randomOrder.get(l));
+      }
+    //System.out.println(randomOrder2);
+
+    ArrayList randomOrder3 = new ArrayList<Integer>();
+      for (int m = 0; m < randomOrder.size(); m++){
+        randomOrder3.add(randomOrder.get(m));
+      }
+    //System.out.println(randomOrder3);
+    System.out.println("\nAscending Array: " + ascend);
+    System.out.println("Bubble Sort Starting");
+    Sorts.bubbleSort(ascend);
+    System.out.println("List after bubble sort: " + ascend );
+
+    System.out.println("\nDescending Array: " + descend);
+    System.out.println("Bubble Sort Starting");
+    Sorts.bubbleSort(descend);
+    System.out.println("List after bubble sort: " + descend );
+
+    System.out.println("\nRandom Array:" + randomOrder);
+    System.out.println("Bubble Sort Starting");
+    Sorts.bubbleSort(randomOrder);
+    System.out.println("List after bubble sort: " + randomOrder );
+
+    System.out.println("\nAscending Array: " + ascend2);
+    System.out.println("Insertion Sort Starting");
+    Sorts.insertionSort(ascend2);
+    System.out.println("List after insertion sort: " + ascend2);
+
+    System.out.println("\nDescending Array: " + descend2);
+    System.out.println("Insertion Sort Starting");
+    Sorts.insertionSort(descend2);
+    System.out.println("List after insertion sort: " + descend2);
+
+    System.out.println("\nRandom Array:" + randomOrder2);
+    System.out.println("Insertion Sort Starting");
+    Sorts.insertionSort(randomOrder2);
+    System.out.println("List after insertion sort:" + randomOrder2);
+
+    System.out.println("\nAscending Array: " + ascend3);
+    System.out.println("Selection Sort Starting");
+    Sorts.bubbleSort(ascend3);
+    System.out.println("List after selection sort: " + ascend3 );
+
+    System.out.println("\nDescending Array: " + descend3);
+    System.out.println("Selection Sort Starting");
+    Sorts.bubbleSort(descend3);
+    System.out.println("List after selection sort: " + descend3 );
+
+    System.out.println("\nRandom Array:" + randomOrder3);
+    System.out.println("Selection Sort Starting");
+    Sorts.selectionSort(randomOrder3);
+    System.out.println("List after selection sort: " + randomOrder3);
+  }
+}
--- Sorts.java
+++ Sorts.java
@@ -1,1 +1,88 @@
+import java.util.ArrayList;
 
+public class Sorts {
+  public static void bubbleSort( ArrayList<Comparable> data )
+  {
+    int passes = 0;
+    int swaps = 0;
+    int comparsions = 0;
+    for (int i = 0; i < data.size() - 1; i++) {
+      passes += 1;
+      for (int j = data.size() - 1; j > i; j--) {
+        comparsions += 1;
+        if (data.get(j).compareTo(data.get(j-1)) < 0) {
+          Comparable temp = data.get(j);
+          data.set(j, data.get(j-1));
+          data.set(j-1, temp);
+          swaps += 1;
+            }
+          }
+        }
+    System.out.println("Number of swaps: " + swaps);
+    System.out.println("Number of passes: " + passes);
+    System.out.println("Number of comparsions: " + comparsions);
+  }
+  public static void selectionSort( ArrayList<Comparable> data )
+{
+  int passes = 0;
+  int swaps = 0;
+  int comparsions = 0;
+  for( int pass = 1; pass<=data.size();pass++ ) {
+    //System.out.println( "\nbegin pass " + (pass) );//diag
+    passes += 1;
+    int maxPos=0;
+    for(int idx = 0;idx<=data.size()-pass;idx++) {
+      //System.out.println( "maxPos: " + maxPos );//diag
+      //System.out.println( data );//diag
+      comparsions += 1;
+      if (data.get(maxPos).compareTo(data.get(idx))<0){
+        maxPos = idx;
+      }
+    }
+    Comparable temp = data.get(maxPos);
+    data.set(maxPos,data.get(data.size()-pass));
+    data.set(data.size()-pass,temp);
+    swaps += 1;
+    //System.out.println( "after swap: " +  data );//diag
+  }
+  System.out.println("Number of swaps: " + swaps);
+  System.out.println("Number of passes: " + passes);
+  System.out.println("Number of comparsions: " + comparsions);
+}
+public static void insertionSort( ArrayList<Comparable> data )
+ {
+   int passes = 0;
+   int swaps = 0;
+   int comparsions = 0;
+   for(int part = 0; part < data.size(); part++  ) {
+     passes += 1;
+     //partition marks first item in unsorted region
+     int partition = part;
+     //System.out.println( "\npartition: " + partition + "\tdataset:"); //diag
+    // System.out.println( data );
+
+     //traverse sorted region from right to left
+     for(int i = part + 1; i > 0; i --  ) {
+       comparsions += 1;
+       // "walk" the current item to where it belongs
+       // by swapping adjacent items
+       if (i == data.size()){
+         break;
+       }
+       else if (data.get(i).compareTo(data.get(i-1)) <0  ) {
+
+         //System.out.println( "swap indices "+(i-1)+" & "+i+"..." ); //diag
+         Comparable temp = data.get(i);
+         data.set(i,data.get(i-1));
+         data.set(i-1,temp);
+         swaps += 1;
+       }
+       else
+         break;
+     }
+   }
+   System.out.println("Number of swaps: " + swaps);
+   System.out.println("Number of passes: " + passes);
+   System.out.println("Number of comparsions: " + comparsions);
+ }
+}
